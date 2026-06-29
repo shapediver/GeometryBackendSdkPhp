d3f916ac4bad23f3f4865c8a6c1e2c79af396ba3
Matthias Reisacher <matt@shapediver.com>
2026-06-29 18:15:44 +0200

    Generate spec version 2.4.0

--- test/AnalyticsApiTest.php
+++ test/AnalyticsApiTest.php
@@ -7,8 +7,6 @@ use ShapeDiver\GeometryApiV2\Client\Api\AnalyticsApi;
 use ShapeDiver\GeometryApiV2\Client\Model\ReqAnyCreditMetricId;
 use ShapeDiver\GeometryApiV2\Client\Model\ReqCreditMetric;
 use ShapeDiver\GeometryApiV2\Client\Model\ReqCreditMetrics;
-use ShapeDiver\GeometryApiV2\Client\Model\ReqModelStatistic;
-use ShapeDiver\GeometryApiV2\Client\Model\ReqModelStatistics;
 use ShapeDiver\GeometryApiV2\Client\Api\SessionApi;
 use ShapeDiver\GeometryApiV2\SdClient;
 use ShapeDiver\GeometryApiV2\SdConfig;
@@ -17,37 +15,6 @@ require_once __DIR__ . '/config.php';
 
 class AnalyticsApiTest extends TestCase
 {
-    public function testModelSessionStatistics(): void
-    {
-        global $host;
-        global $jwtModel;
-        global $modelId;
-
-        $client = new SdClient();
-        $modelConfig = (new SdConfig())->setHost($host)->setAccessToken($jwtModel);
-
-        // Initialize a new session.
-        $ticket = TestUtils::createTicket();
-        $sessionId = (new SessionApi($client, $modelConfig))
-            ->createSessionByTicket($ticket)
-            ->getSessionId();
-
-        # Fetch model statistics within a specific time range.
-        $reqStats = (new ReqModelStatistics())->setParameters(
-            [
-                (new ReqModelStatistic())
-                    ->setModelid([$modelId])
-                    ->setTimestampFrom("2024")
-                    ->setTimestampTo("2025")
-            ]
-        );
-        $resStats = (new AnalyticsApi($client, $modelConfig))->getModelStatistics($reqStats);
-        $this->assertNotEmpty($resStats->getAnalytics()->getModels());
-
-        // Close the session.
-        (new SessionApi($client, $modelConfig))->closeSession($sessionId);
-    }
-
     public function testCreditMetrics(): void
     {
         global $host;
